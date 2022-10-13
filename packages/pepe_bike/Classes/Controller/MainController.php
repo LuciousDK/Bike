acd8a8bc1cfd096b6361aeeb99b93a27009a1c6a
Luat Dinh Bui <l.dinh-bui@ovan.de>
2022-10-13 15:13:21 +0200

    Constants & setup

--- packages/pepe_bike/Classes/Controller/MainController.php
+++ packages/pepe_bike/Classes/Controller/MainController.php
@@ -4,12 +4,45 @@ declare (strict_types = 1);
 
 namespace Luat\PepeBike\Controller;
 
+use \Luat\PepeBike\Domain\Repository\BicycleRepository;
+use \Luat\PepeBike\Domain\Repository\BrandRepository;
+
 /**
  * MainController
  */
 class MainController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 
 {
+
+    /**
+     * @var BicycleRepository
+     */
+    protected $bicycleRepository;
+
+    /**
+     * Inject a bicycle repository
+     *
+     * @param BicycleRepository $bicycleRepository
+     */
+    public function injectBicycleRepository(BicycleRepository $bicycleRepository)
+    {
+        $this->bicycleRepository = $bicycleRepository;
+    }
+
+    /**
+     * @var BrandRepository
+     */
+    protected $brandRepository;
+
+    /**
+     * Inject a brand repository
+     *
+     * @param BrandRepository $brandRepository
+     */
+    public function injectBrandRepository(BrandRepository $brandRepository)
+    {
+        $this->brandRepository = $brandRepository;
+    }
 
     /**
      * Init the actions
@@ -34,6 +67,8 @@ class MainController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
      */
     public function listAction()
     {
+        $this->view->assign('bicycles', $this->bicycleRepository->findAll());
+        $this->view->assign('brand', $this->brandRepository->findAll());
     }
 
 }
