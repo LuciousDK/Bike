1c9ffc65fcac4091d3bc3419272f52ef70d49445
Luat Dinh Bui <l.dinh-bui@ovan.de>
2022-11-08 11:17:43 +0100

    Additional repository functions

--- packages/pepe_bike/Classes/Domain/Repository/BicycleRepository.php
+++ packages/pepe_bike/Classes/Domain/Repository/BicycleRepository.php
@@ -1,8 +1,9 @@
 <?php
-declare(strict_types = 1);
+declare (strict_types = 1);
 
 namespace Luat\PepeBike\Domain\Repository;
 
+use Luat\PepeBike\Domain\Model\Bicycle;
 use Luat\PepeBike\Domain\Model\Brand;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
@@ -30,7 +31,7 @@ class BicycleRepository extends Repository
      */
     public function findAllIncludingHiddenAndDeleted()
     {
-        $query = $this->createQuery(); 
+        $query = $this->createQuery();
         $query->getQuerySettings()
             ->setIgnoreEnableFields(true)
             ->setEnableFieldsToBeIgnored(['disabled'])
@@ -43,14 +44,51 @@ class BicycleRepository extends Repository
      * @throws \TYPO3\CMS\Core\Context\Exception\AspectNotFoundException
      * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
      */
+    public function findByUidIncludingHidden(int $uid)
+    {
+        $query = $this->createQuery();
+        $query->getQuerySettings()
+            ->setIgnoreEnableFields(true)
+            ->setEnableFieldsToBeIgnored(['disabled']);
+        $constraints = [];
+        $constraints[] = $query->equals('uid', (int) $uid);
+        return $query->matching($query->logicalAnd($constraints))->execute()->getFirst();
+    }
+    /**
+     * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+     * @throws \TYPO3\CMS\Core\Context\Exception\AspectNotFoundException
+     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
+     */
     public function findAllIncludingHidden()
     {
-        $query = $this->createQuery(); 
+        $query = $this->createQuery();
         $query->getQuerySettings()
             ->setIgnoreEnableFields(true)
             ->setEnableFieldsToBeIgnored(['disabled']);
         return $query->execute();
     }
 
+    /**
+     * @param int|Bicycle $bicycle
+     * @param null|bool $bicycle
+     * @return array|\TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+     * @throws \TYPO3\CMS\Core\Context\Exception\AspectNotFoundException
+     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
+     */
+    public function changeHiddenStatus($bicycle, ?bool $status = null): ?Bicycle
+    {
+        if (is_int($bicycle)) {
+            $bicycle = $this->findByUidIncludingHidden((int)$bicycle);
+        }
+        if (!($bicycle instanceof Bicycle) || $bicycle == null) {
+            return null;
+        }
+        if ($status == null) {
+            $status = !$bicycle->getHidden();
+        }
 
+        $bicycle->setHidden($status);
+        return $bicycle;
+
+    }
 }
