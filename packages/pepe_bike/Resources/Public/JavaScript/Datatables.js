c0fc15549dcf529510de35e1834bad7d68efbc6f
Luat Dinh Bui <l.dinh-bui@ovan.de>
2022-11-08 11:16:59 +0100

    Onchage value request

--- packages/pepe_bike/Resources/Public/JavaScript/Datatables.js
+++ packages/pepe_bike/Resources/Public/JavaScript/Datatables.js
@@ -3,13 +3,27 @@
 * Module: TYPO3/CMS/PepeBike/Datatables
 */
 define(['jquery', 'TYPO3/CMS/Core/Ajax/AjaxRequest'], function ($, AjaxRequest) {
-  let request = new AjaxRequest(TYPO3.settings.ajaxUrls.hide_bike)
-    .withQueryArguments({ uid: 1, hidden: true })
-    .get()
-    .then(async function (response) {
-      const resolved = await response.resolve();
-      console.log(response);
-    });
+  // console.log(TYPO3.settings.ajaxUrls.hide_bike);
+  // let request = new AjaxRequest(TYPO3.settings.ajaxUrls.hide_bike)
+  //   .withQueryArguments({ uid: 1, hidden: true })
+  //   .get()
+  //   .then(async function (response) {
+  //     const resolved = await response.resolve();
+  //     console.log(response);
+  //   });
+
+  document.querySelectorAll('.hidden-toggler').forEach(element => {
+    element.addEventListener('change', () => {
+      let uri = element.dataset.requestUri.replace('__status__',element.checked);
+      console.log(uri)
+      let request = new AjaxRequest(uri)
+        .get()
+        .then(async function (response) {
+          const resolved = await response.resolve();
+          console.log(response);
+        });
+    })
+  })
 });
 
 
