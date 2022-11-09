25af4631b10c7b7ebe571da1e46faeb66d409c1f
Luat Dinh Bui <l.dinh-bui@ovan.de>
2022-11-09 10:27:46 +0100

    Export tsconfig

---
+++ packages/pepe_bike/Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+   'pepebike',
+   'Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig',
+   'Pepe Bike ContentElements'
+);
--- packages/pepe_bike/ext_localconf.php
+++ packages/pepe_bike/ext_localconf.php
@@ -5,9 +5,6 @@ if (!defined('TYPO3_MODE')) {
 
 $_EXTKEY = 'pepebike';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pepebike/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig">'
-);
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Luat.PepeBike',
