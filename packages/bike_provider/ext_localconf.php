518671d879b11228d7ce148bab060abdf147bb70
Luat Dinh Bui <l.dinh-bui@ovan.de>
2022-11-16 14:50:01 +0100

    Icon register

--- packages/bike_provider/ext_localconf.php
+++ packages/bike_provider/ext_localconf.php
@@ -9,3 +9,11 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['bike_provider'] = 'EXT:bike_provi
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bike_provider/Configuration/TsConfig/Page/All.tsconfig">');
+
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+
+$iconRegistry->registerIcon(
+    'image',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:bike_provider/Resources/Public/Icons/image.svg']
+);
